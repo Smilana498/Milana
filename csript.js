ca1721cfec817183b02e6337929f2e82a405b052
Milana <mm6085058@gmail.com>
2024-11-13 12:03:42 +0600

    first commit

---
+++ csript.js
@@ -0,0 +1,27 @@
+document.getElementById("MuButton").addEventListener("click", function() {
+    window.location.href="https://wa.me/996550078888";// ссылка на WhatsApp
+});
+
+document.getElementById("MuButto").addEventListener("click", function() {
+    window.location.href="https://www.facebook.com/kfckg";// ссылка на Facebook
+});
+
+document.getElementById("MuButt").addEventListener("click", function() {
+    window.location.href="https://www.instagram.com/kfc.kg?igsh=MWExOXN0cWg3MXdhMQ==";// ссылка на Instafram
+});
+
+document.getElementById("muButton").addEventListener("click", function() {
+    alert("Ваш заказ оформлен")
+});
+document.getElementById("muButto").addEventListener("click", function() {
+    alert("Ваш заказ оформлен")
+});
+document.getElementById("muButt").addEventListener("click", function() {
+    alert("Ваш заказ оформлен")
+});
+document.getElementById("muBut").addEventListener("click", function() {
+    alert("Ваш заказ оформлен")
+});
+document.getElementById("muBu").addEventListener("click", function() {
+    alert("Ваш заказ оформлен")
+});
